f44fadb746a1aab4e0c6bf3c7fab9293dd96cb4a
brunostuessy <bruno.stuessi@gmail.com>
2024-11-05 11:09:42 +0100

    Initial commit

---
+++ SpringBootApp1/src/main/java/io/brunostuessi/AppConfiguration.java
@@ -0,0 +1,61 @@
+package io.brunostuessi;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+@Configuration(proxyBeanMethods = false)
+public class AppConfiguration {
+
+    private static Logger LOG = LoggerFactory.getLogger(App.class);
+
+    @Bean()
+    public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+        return args -> {
+            LOG.info("Let's inspect the beans provided by Spring Boot:");
+
+            String[] beanNames = ctx.getBeanDefinitionNames();
+            Arrays.sort(beanNames);
+            for (String beanName : beanNames) {
+                LOG.info(beanName);
+            }
+
+            LOG.info("Let's sleep 5 secs");
+
+            Thread.sleep(5 * 1000);
+
+            LOG.info("Let's run 1k sleep 10 ms tasks in virtual threads");
+
+            try (ExecutorService executor = Executors.newVirtualThreadPerTaskExecutor()) {
+                for (int i = 0; i < 1000; i++) {
+                    executor.submit(() -> {
+                        try {
+                            Thread.sleep(10);
+                        } catch (InterruptedException e) {
+                            throw new RuntimeException(e);
+                        }
+                    });
+                }
+                executor.shutdown();
+                LOG.info("" + executor.awaitTermination(5, TimeUnit.MINUTES));
+            }
+
+            LOG.info("Let's run AeronSimple");
+
+            AeronSimple.main(null);
+
+            LOG.info("Terminating");
+        };
+    }
+
+}
